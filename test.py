0b4e152c75ee5bcf99a82de93927e758f2d6c90d
adityashah-afk <aditya.shah.ventures@gmail.com>
2026-06-27 11:28:12 +0530

    Test live deployment
    Changed discount calculation to use 90 instead of 100.

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 def calculate_discount(price, discount_percent):
-    discount_amount = price * (discount_percent / 100)
+    discount_amount = price * (discount_percent / 90)
     final_price = price - discount_amount
     return final_price
+    # Testing the live bot!
